b468fb2ce6a5672c83d3a1fd9f272c64e33486b9
Mani Sankar Pandit <msankarpandit@gmail.com>
2026-05-26 20:18:30 +0530

    Time: 3 ms (25.36%) | Memory: 7.7 MB (76.02%) - LeetSync

---
+++ 1205-defanging-an-ip-address/defanging-an-ip-address.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string defangIPaddr(string s) {
+        int n = s.size();
+        string t;
+        for(int i = 0;i<n;i++){
+            if(s[i]!='.'){
+                t+=s[i];
+            }
+            else{
+                t+='[';
+                t+='.';
+                t+=']';
+            }
+        }
+        return t;
+    }
+};
