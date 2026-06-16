13e29baada0cbeeec5271b72816790a9076512ae
Mani Sankar Pandit <msankarpandit@gmail.com>
2026-06-16 10:37:07 +0530

    Time: 0 ms (100.00%) | Memory: 39.7 MB (87.50%) - LeetSync

---
+++ 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string processStr(string s) {
+        int n = s.size();
+        string ans = "";
+        for (int i = 0; i < n; i++) {
+            if (s[i] >= 'a' && s[i] <= 'z') ans.push_back(s[i]);
+            else if (s[i] == '%') reverse(ans.begin(), ans.end());
+            else if (s[i] == '#') ans += ans;
+            else if (s[i] == '*') {
+                if (!ans.empty())
+                    ans.pop_back();
+            }
+        }
+        return ans;
+    }
+};
