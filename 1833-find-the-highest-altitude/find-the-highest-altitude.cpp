98d4dbc939652a821666cc7128b1fff2a58764de
Mani Sankar Pandit <msankarpandit@gmail.com>
2026-06-19 11:38:18 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (44.68%) - LeetSync

--- 1833-find-the-highest-altitude/find-the-highest-altitude.cpp
+++ 1833-find-the-highest-altitude/find-the-highest-altitude.cpp
@@ -4,16 +4,10 @@ public:
         int n = gain.size();
         int sum = 0;
         int mx = 0;
-        // vector<int>ans;
-        // ans.push_back(sum);
         for(int i = 0;i<n;i++){
             sum+=gain[i];
-            // ans.push_back(sum);
             mx = max(mx,sum);
         }
-        // for(int i = 0;i<ans.size();i++){
-        //     mx = max(mx,ans[i]);
-        // }
         return mx;
     }
 };
