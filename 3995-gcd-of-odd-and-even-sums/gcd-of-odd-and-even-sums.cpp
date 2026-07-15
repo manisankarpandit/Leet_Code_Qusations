e3688802154c91a49678d19dd7a7c9377940100b
Mani Sankar Pandit <msankarpandit@gmail.com>
2026-07-15 21:01:23 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (85.02%) - LeetSync

---
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int gcdOfOddEvenSums(int n) {
+        int sOdds = n*n;
+        int sEvens = (n*n)+n;
+        while(sEvens){
+            int rem = sOdds % sEvens;
+            sOdds = sEvens;
+            sEvens = rem;
+        }
+        return sOdds;
+    }
+};
