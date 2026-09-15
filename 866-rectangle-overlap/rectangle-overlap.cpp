aa3b5437c2f03573197c978b9338b7997d66c09f
Mani Sankar Pandit <msankarpandit@gmail.com>
2026-09-15 18:53:07 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (46.89%) - LeetSync

--- 866-rectangle-overlap/rectangle-overlap.cpp
+++ 866-rectangle-overlap/rectangle-overlap.cpp
@@ -3,7 +3,6 @@ public:
     bool isRectangleOverlap(vector<int>& r1, vector<int>& r2) {
         int ax1 = r1[0], ay1 = r1[1] , ax2 = r1[2] , ay2 = r1[3];
         int bx1 = r2[0], by1 = r2[1] , bx2 = r2[2] , by2 = r2[3];
-        // int overlap_area = (min(ax2,bx2)-max(ax1,bx1))*(min(ay2,by2)-max(ay1,by1));
         if(ax2>bx1 && ax1<bx2 && ay2>by1 && ay1<by2) return true;
         return false;
     }
