c23b514a6ae746d6412ec454d56f8cd4eec8f286
Mani Sankar Pandit <msankarpandit@gmail.com>
2026-09-20 12:56:25 +0530

    Time: 3 ms (16.14%) | Memory: 9.6 MB (78.39%) - LeetSync

--- 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.cpp
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.cpp
@@ -4,10 +4,11 @@ public:
         int n = s.size();
         int sum = 0;
         for(int i = 0;i<n;i++){
-            int x = int(s[i]);
-            int idx = (123-x);
-            int product = idx*(i+1);
-            sum += product;
+            // int x = int(s[i]);
+            // int idx = (123-x);
+            // int product = idx*(i+1);
+            //sum+=product;
+            sum += (123-int(s[i]))*(i+1);
         }
         return sum;
     }
